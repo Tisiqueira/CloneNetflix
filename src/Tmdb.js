992ff63db83be33e5b082402782166a2afe32c80
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2020-09-13 00:14:55 -0300

    Update Tmdb.js

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -58,10 +58,10 @@ export default {
         if(movieId) {
             switch(type){
                 case 'movie' :
-                    info = await basicFetch(` /movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    info = await basicFetch(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
                 break;
                 case 'tv':
-                    info = await basicFetch(` /tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                    info = await basicFetch(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
                 break;
                 default:
                     info = null;
