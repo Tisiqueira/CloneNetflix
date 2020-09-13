bceb1b3ff6af4f77f19cc5f354784f585e814a79
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2020-09-12 21:01:58 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+export default ( ) => {
+
+  const [movieList, setMovieList] = useState ([]);
+  const [FeaturedData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect (() =>{
+
+    const loadAll = async () => {
+
+      //Get list all
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);  
+
+      //Add o Featured
+      let originals = list.filter(i=>i.slug ==='originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData(chosenInfo);
+      
+    }
+    loadAll();
+
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      }else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll',scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
+  return (
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+      {FeaturedData &&
+        <FeaturedMovie item={FeaturedData}/>
+      }
+
+      <section className="lists">
+        {movieList.map((item, Key) =>(
+          <MovieRow Key={Key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+      <footer>
+        <h4>Feito com <span role="img" aria-label="coração">❤</span> pela B7Web </h4>
+        <h4>Direitos de imagem para Netflix</h4>
+        <h4>Dados pegos do site Themoviedb.org</h4>
+      </footer>
+    </div>
+  );
+}
