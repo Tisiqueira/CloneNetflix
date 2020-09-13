7989b0743e32cce6ca7cdbaa052a6c95d5fa29d1
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2020-09-13 00:03:55 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,13 @@ export default ({black}) => {
         <header className={black ? 'black' : ''}>
             <div className="header--logo">
                 <a href="/">
-                    <img src="https://upload.wikipedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix"></img>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix" />
+                             
                 </a>
             </div>
             <div className="header--user">
                 <a href="/">
-                    <img src="pbs.twing.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Usuário"></img>
+                    <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Usuário" />   
                 </a>
             </div>
         </header>
